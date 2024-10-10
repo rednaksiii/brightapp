2bcc93016b6005478b34fcabd01cc098d8a3cb57
nedia <113921954+cereal-with-water@users.noreply.github.com>
2024-10-10 10:33:42 -0400

    Update auth_controller.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,9 +1,13 @@
 // lib/controllers/auth_controller.dart
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthController extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FacebookAuth _facebookAuth = FacebookAuth.instance;
   User? user;
 
   AuthController() {
@@ -11,6 +15,76 @@ class AuthController extends ChangeNotifier {
       this.user = user;
       notifyListeners();
     });
+  }
+
+  Future<void> signInWithEmail(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? 'An error occurred during sign-in.';
+    }
+  }
+
+  // Google login
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      if (googleUser == null) {
+        return;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? 'unexpected error.';
+    } catch (e) {
+      throw 'Unexpected error occurred while signing you in.';
+    }
+  }
+
+  /// Facebook login
+  Future<void> signInWithFacebook() async {
+    try {
+      final LoginResult result = await _facebookAuth.login();
+
+      if (result.status == LoginStatus.success) {
+        final AccessToken accessToken = result.accessToken!;
+        final OAuthCredential credential =
+            FacebookAuthProvider.credential(accessToken.tokenString);
+
+        await _auth.signInWithCredential(credential);
+      } else if (result.status == LoginStatus.cancelled) {
+        throw Exception('Facebook login cancelled');
+      } else {
+        throw Exception(result.message ?? 'unexpected error.');
+      }
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.message ?? 'unexpected error.');
+    } catch (e) {
+      throw Exception('An error occurred while signing in.');
+    }
+  }
+
+  // logout
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+      await _googleSignIn.signOut();
+      await _facebookAuth.logOut();
+    } catch (e) {
+      throw 'An error occurred while signing out.';
+    }
   }
 
   // Additional authentication methods can be added here
