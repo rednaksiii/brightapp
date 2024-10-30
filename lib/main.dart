da8ee021eb4f5f6b8d436a106159db21430c5b72
nedia <113921954+cereal-with-water@users.noreply.github.com>
2024-10-30 15:51:42 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:brightapp/controllers/auth_controller.dart';
 import 'package:brightapp/routes.dart';
-import '/api/firebase_api.dart'; //Aylin added this
+import 'package:brightapp/pages/home/home_page_ui.dart';
+import 'package:brightapp/pages/login/login_page_ui.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
-  await FirebaseApi().initNotifications(); //Aylin added this
-
   runApp(const BrightApp());
 }
 
@@ -26,9 +24,32 @@ class BrightApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: '/login', // Set the initial route
+        initialRoute: '/home',
+        //home: const InitialScreen(), // Set the initial route
         routes: routes, // Define your routes here
       ),
+    );
+  }
+}
+
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthController>(
+      builder: (context, authController, child) {
+        // Check if login status is determined
+        if (authController.user == null && !authController.isUserLoggedIn()) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (authController.isUserLoggedIn()) {
+          // User is logged in, navigate to home screen
+          return const HomePageUI();
+        } else {
+          // User is not logged in, navigate to login screen
+          return const LoginPageUI();
+        }
+      },
     );
   }
 }
